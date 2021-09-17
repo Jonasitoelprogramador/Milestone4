2bc80b1c4507a38d44a2f1a318f162a58bfeacc8
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2021-09-17 12:29:15 +0000

    remove attributes from the offering model that will be moved to the host model

--- hostofferings/forms.py
+++ hostofferings/forms.py
@@ -5,5 +5,5 @@ from .models import offering
 class OfferingForm(forms.ModelForm):
     class Meta:
         model = offering
-        fields = ('name', 'nationality', 'first_language', 'location', 'work_category', 'work_details', 'email') 
+        fields = ('work_category', 'work_details') 
 
--- hostofferings/models.py
+++ hostofferings/models.py
@@ -5,13 +5,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class offering(models.Model):
-    name = models.CharField(max_length=100)
-    nationality = CountryField()
-    first_language = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
+    #name = models.CharField(max_length=100)
+    #nationality = CountryField()
+    #first_language = models.CharField(max_length=100)
+    #location = models.CharField(max_length=100)
     work_category = models.CharField(max_length=100)
     work_details = models.TextField()
-    email = models.EmailField(max_length=100)
+    #email = models.EmailField(max_length=100)
     host = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
 
