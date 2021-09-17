3ee23cc080d6ec9ced0718dbb22dda890156756e
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2021-09-17 12:22:37 +0000

    add edit_offering function to views that allows the user to edit a given offering

--- Milestone4/urls.py
+++ Milestone4/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from accounts.views import register, login, profile, logout
-from hostofferings.views import add_offering, all_offerings, offering_details
+from hostofferings.views import add_offering, all_offerings, offering_details, delete_offering, edit_offering
 
 
 urlpatterns = [
@@ -28,5 +28,7 @@ urlpatterns = [
     url(r'^logout/$', logout, name='logout'),
     url(r'^add_offering/$', add_offering, name='add_offering'),
     url(r'^all_offerings/$', all_offerings, name='all_offerings'),
-    url(r'^offering_details/(?P<pk>\d+)/$', offering_details, name="offering_details")
+    url(r'^offering_details/(?P<pk>\d+)/$', offering_details, name="offering_details"),
+    url(r'^delete/(?P<pk>\d+)/$', delete_offering, name='delete_offering'),
+    url(r'^edit_offering/(?P<pk>\d+)/$', edit_offering, name='edit_offering')
 ]
--- hostofferings/views.py
+++ hostofferings/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from .forms import OfferingForm
 from django.contrib.auth.decorators import login_required
 from .models import offering
 
 # Create your views here.
 
+
 @login_required()  
 def add_offering(request):
     if request.method == "POST":
-        form = offeringForm(request.POST)
+        form = OfferingForm(request.POST)
         if form.is_valid():
             offering = form.save(commit=False)
             offering.host = request.user
@@ -17,7 +18,7 @@ def add_offering(request):
         else:
             return HttpResponse("data not valid")
     else:
-        form = offeringForm()
+        form = OfferingForm()
     return render(request, 'hostofferings/add_offering.html', {'form': form})
 
 
@@ -32,5 +33,28 @@ def offering_details(request, pk):
     offering_details = get_object_or_404(offering, pk=pk)
     print(offering_details)
     return render(request, 'hostofferings/offering_details.html', {'offering_details': offering_details})
+
+
+@login_required() 
+def delete_offering(request, pk):
+    current_offering = get_object_or_404(offering, pk=pk) 
+    current_offering.delete()
+    return redirect('profile')
     
-    
+
+@login_required()  
+def edit_offering(request, pk):
+    if request.method == "POST":
+        form = OfferingForm(request.POST)
+        if form.is_valid():
+            current_offering = form.save(commit=False)
+            current_offering.host = request.user
+            current_offering.save()
+            return HttpResponse("edited!")
+        else:
+            return HttpResponse("data not valid")
+    else:
+        offer = offering.objects.get(pk=pk)
+        form = OfferingForm(instance=offer)
+    return render(request, 'hostofferings/add_offering.html', {'form': form})
+
