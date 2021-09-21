c38cdb4b89414d36da848db9733dd41e757e53e0
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2021-09-21 12:34:29 +0000

    define 2 functions with receiver decoarator to attempt to create a Host instance everytime a user instance is created

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Host(models.Model):
@@ -23,3 +24,13 @@ class Worker(models.Model):
 
     def __str__(self):
         return self.user
+
+@receiver(post_save, Sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Host.objects.create(user=instance)
+
+
+@receiver(post_save, Sender=User)
+def save_user_profile(instance, **kwargs):
+    instance.profile.save()
