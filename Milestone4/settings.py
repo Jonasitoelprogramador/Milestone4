f35c52c8d2002727b3d0c73daceaad9b7c4d8060
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2021-10-07 16:13:17 +0000

    add new html and css to register.html to improve UX

--- Milestone4/settings.py
+++ Milestone4/settings.py
@@ -132,7 +132,7 @@ MEDIA_URL = '/media/'
 
 LOGIN_URL = 'login'
 
-STATIC_ROOT = 'c:/milestone4_cdn'
+STATIC_ROOT = '../milestone4_cdn'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
