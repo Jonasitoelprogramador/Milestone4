f9199203f52ccc46e7f753997ec05ac6b1caa0d6
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2022-02-25 11:56:18 +0000

    redefine location variable in custo_storages

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,8 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
-from .settings import STATICFILES_LOCATION, MEDIAFILES_LOCATION
 
 class StaticStorage(S3Boto3Storage):
-    location = STATICFILES_LOCATION
-
+    location = 'static'
 
 class MediaStorage(S3Boto3Storage):
-    location = MEDIAFILES_LOCATION
+    location = 'media'
