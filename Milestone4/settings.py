a0a3e9a12a4fb969cc5cb0c2fd0139eb1b42951f
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2022-02-25 21:57:03 +0000

    add whitenoise

--- Milestone4/settings.py
+++ Milestone4/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Milestone4.urls'
@@ -131,7 +132,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+'''STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -141,7 +156,7 @@ MEDIA_URL = '/media/'
 #This will only run if USE_AWS is included in the environment variables
 
 
-'''if 'USE_AWS' in os.environ:
+if 'USE_AWS' in os.environ:
     # Cache control	
     AWS_S3_OBJECT_PARAMETERS = {	
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',	
