5a4ccc21debf756b6b143205b5934ddc9ea98403
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2022-02-26 09:48:40 +0000

    troubleshoot

--- Milestone4/settings.py
+++ Milestone4/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage''''
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'''
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
