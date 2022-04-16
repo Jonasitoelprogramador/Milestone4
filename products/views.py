a3e87330813f4d55e58526e1b38ac781aaed5443
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2022-04-16 17:06:50 +0000

    pass through pk value of product instance to template

--- products/views.py
+++ products/views.py
@@ -92,12 +92,18 @@ def StripeWebhook(request):
 
 def ProductView(request):
     forms_list = []
+    if request.method == 'POST':
+        product_instance = Product.objects.get(id=current_user)
+        namey = request.POST.getlist('name')
+        print(f'It is you Im looking for babe {type(namey[0])}')
+        #product_filled_form = ProductForm(request.POST, instance=)
+        #product_filled_form.save()
     instances = list(Product.objects.all())
     # loop through all the instances
     for i in range(len(instances)):
         # get each instance
         instancey = Product.objects.get(name=instances[i])
-        print(f'this is the instance: {type(instancey)}')
+        print(f'this is the instance:  {instancey.pk}')
         # bind each instance to a form
-        forms_list.append(ProductForm(instance=instancey))
+        forms_list.append([ProductForm(instance=instancey), instancey.pk])
     return render(request, "products/product-admin.html", {'forms_list': forms_list})
